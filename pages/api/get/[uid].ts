69f7651a7e00f82b52351c0dea0bede4044960f8
VimForLaNie <new.giant098@gmail.com>
2022-03-20 13:27:49 +0700

    force uid to string

--- pages/api/get/[uid].ts
+++ pages/api/get/[uid].ts
@@ -3,14 +3,13 @@ import { fireStore, fireAuth } from 'utils/firebase';
 
 const handler = async (req:NextApiRequest, res:NextApiResponse) => {
     const { uid } = req.query;
-    const data = await fireStore.collection('user').doc(uid).get();
+    const data = await fireStore.collection('user').doc(String(uid)).get();
     if(data.data()){
         res.status(200).json(data.data());
     }
     else {
         res.status(404).json(JSON.stringify("uid not found"));
     }
-    
 }
 
 export default handler;
