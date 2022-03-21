f4d39e7fd469fc47a400f22281b61140da102144
VimForLaNie <new.giant098@gmail.com>
2022-03-21 10:59:33 +0700

    improved rmTransaction

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,12 @@ import { fireAuth, fireStore } from 'utils/firebase';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-const user = fireAuth.currentUser;
-
 const Home: NextPage = (props) => {
   const [Auth, setAuth] = useState(true);
   const [Uid, setUid] = useState("");
   const [data, setData] = useState<any>();
   const [amount, setAmount] = useState("");
-  const [Index, setIndex] = useState("-1");
+  const [Index, setIndex] = useState(0);
 
   useEffect(() => {
    const set = async () => {
@@ -43,23 +41,29 @@ const Home: NextPage = (props) => {
     let temp = data?.transactions;
     console.log(temp);
     if(!temp) { console.log("fuck"); return; }
-    const i = parseInt(Index);
-    if(i !== NaN && i > -1) temp.splice(i - 1, 1)
+    if(Index !== NaN && Index > -1) temp.splice(Index - 1, 1)
     // setData(temp);
     fireStore.collection('user').doc(String(Uid)).update({
       transactions : temp,
     }).then(() => console.log("yay"));
 
-    setIndex(""); //reset 
+    setIndex(0); //reset 
   }
 
-  if(Auth){
+  if(Auth){ 
     return ( 
       <>
         <div> 
           <input type="text" id="amount" placeholder='amount' value={amount} onChange={e => setAmount(e.currentTarget.value)}></input>
           <input type="button" value="Submit" onClick={addTransaction}/>
-          <input type="text" id="index" placeholder='which one to remove' value={Index} onChange={e => setIndex(e.currentTarget.value)}></input>
+          <input 
+            type="number" 
+            id="index" 
+            min="0" max={data?.transactions.length - 1}
+            placeholder='which one to remove' 
+            value={Index} 
+            onChange={e => setIndex(parseInt(e.currentTarget.value))}>
+          </input>
           <input type="button" value="Send" onClick={rmTransaction}/>
         </div>
         <div>
@@ -72,7 +76,7 @@ const Home: NextPage = (props) => {
       </> 
     );
   }
-  else{
+  else {
     return (<Link href="/login">Go to Login</Link>);
   }
 }
