e6fff461c10e0b0478d654e4c807e62937eeb95c
VimForLaNie <new.giant098@gmail.com>
2022-03-21 12:19:08 +0700

    data restructure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,37 @@ import { fireAuth, fireStore } from 'utils/firebase';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
+interface transaction {
+  to : string,
+  amount : number,
+  date : string,
+}
+interface wallet {
+  name : string,
+  value : number,
+  transactions : transaction[],
+}
+
 const Home: NextPage = (props) => {
   const [Auth, setAuth] = useState(false);
   const [Uid, setUid] = useState("");
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<wallet[]>([{name : "", value : 0, transactions : [{to : "", amount : 0, date : ""}]}]);
   const [amount, setAmount] = useState("");
-  const [Index, setIndex] = useState(1);
+  const [Index, setIndex] = useState(0);
+  const [Names, setNames] = useState<string[]>();
+  const [Rm, setRm] = useState(1);
 
   const db = fireStore.collection('user');
 
   useEffect(() => {
    const set = async () => {
-     setData((await db.doc(String(Uid)).get()).data()?.wallets)
+     let temp:wallet[] = (await fireStore.collection('user').doc(String(Uid)).get()).data()?.wallets;
+     let names = temp.map(({name}) => {return name});
+     setData(temp);
+     setNames(names);
    }
    if(Uid) set();
-  },[Uid,db]);
+  },[Uid]);
 
   fireAuth.onAuthStateChanged((user) => {
     if(user){
@@ -33,7 +49,8 @@ const Home: NextPage = (props) => {
   const addTransaction = async () => {
     const Cash = data[0];
     Cash.value += parseInt(String(amount));
-    Cash.transactions = [...Cash.transactions, amount]; 
+    let temp:transaction = { to : "Cash", amount : parseInt(String(amount)), date: String(new Date()) };
+    Cash.transactions = [...Cash.transactions, temp]; 
     setData([Cash]); //need fix
     db.doc(String(Uid)).update({
       wallets : data,
@@ -50,7 +67,7 @@ const Home: NextPage = (props) => {
   
     if(!temp) { console.log("no"); return; }
     
-    if(Index > -1) change = -temp.splice(Index - 1, 1);
+    if(Rm > -1) change = -temp.splice(Rm - 1, 1);
 
     Cash.transactions = temp;
     Cash.value += change;
@@ -60,7 +77,7 @@ const Home: NextPage = (props) => {
       wallets : data,
     }).then(() => console.log("yay"));
     
-    setIndex(1); //reset 
+    setRm(1); //reset 
   }
 
   if(Auth){ 
@@ -74,16 +91,18 @@ const Home: NextPage = (props) => {
             id="index" 
             min="1" max={data[0]?.transactions.length}
             placeholder='which one to remove' 
-            value={Index} 
-            onChange={e => setIndex(parseInt(e.currentTarget.value))}>
+            value={Rm} 
+            onChange={e => setRm(parseInt(e.currentTarget.value))}>
           </input>
           <input type="button" value="Send" onClick={rmTransaction}/>
         </div>
         <h1>{data[0]?.value}</h1>
         <div>
           {
-            data[0]?.transactions.map((e:number,i:number)=>{
-              return <p key={i}>{e}</p>
+            data[0]?.transactions.map((e:transaction,i:number)=>{
+              return <div key={i}>
+                <p>to : {e.to}</p> <p> for : {e.amount}</p> <p> @{e.date}</p> <br></br>
+              </div>
             })
           }
         </div>
