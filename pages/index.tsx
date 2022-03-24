771875891e5a4cc5d4a512441bab9a0001695f4c
VimForLaNie <new.giant098@gmail.com>
2022-03-24 21:04:17 +0700

    caching!

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import crypto from 'crypto';
 
 import { fireAuth } from 'utils/firebase';
 import { userCtx, currIndexCtx, walletNamesCtx } from 'utils/Context';
@@ -20,16 +21,30 @@ const Home: NextPage = (props) => {
   const currentIndexCtxValue = { currentIndex, setCurrentIndex };
   const walletNamesCtxValue = { walletNames };
   
+  const hashCode = (str:string) => {
+    return str.split('').reduce((prevHash, currVal) => (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+  }
+
   useEffect(() => {
     const set = async () => {
-      let temp:wallet[] = (await (await (fetch(`/api/get/${userUid}`))).json()).wallets;
-      if(!temp) return;
-      let names = temp.map(({name},i) => {return name;});
-      setUserData(temp);
-      setWalletNames(names);
+      const cache = window.localStorage.getItem(`data:${hashCode(userUid)}`);
+      if(cache === null) {
+        let temp:wallet[] = (await (await (fetch(`/api/get/${userUid}`))).json()).wallets;
+        let names = temp.map(({name},i) => {return name;});
+        window.localStorage.setItem(`data:${hashCode(userUid)}`, JSON.stringify(temp));
+        setUserData(temp);
+        setWalletNames(names);
+        console.log("caching");
+      }
+      else{
+        let data:wallet[] = JSON.parse(cache);
+        setUserData(data);
+        setWalletNames(data.map(({name},i) => { return name;}));
+        console.log("use caches");
+      }
     }
+    
     if(userUid) set();
-
     console.log("useEffect!");
    },[userUid]);
 
