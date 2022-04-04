a6b68555c717703e8501a1a25fb3dc4c35c2320e
VimForLaNie <new.giant098@gmail.com>
2022-04-04 14:40:19 +0700

    card redesign

--- components/transactionCard.tsx
+++ components/transactionCard.tsx
@@ -1,16 +1,17 @@
 import moment from "moment-timezone";
 import { FC } from "react";
-import styles from 'styles/components/Card.module.css';
 
 const Card:FC<transaction> = (props) => {
-
+    const time = moment.unix(props.date).tz(`${Intl.DateTimeFormat().resolvedOptions().timeZone}`).toObject();
     return(
-        <div className=" max-w-xl bg-blue-500 rounded-2xl p-4 m-2 self-center hover-float text-slate-50 transition-all">
-            <p>from : {props.from}</p>
-            <p>to : {props.to}</p> 
-            <p>for : {props.amount}</p> 
-            <p>@{moment.unix(props.date).tz(`${Intl.DateTimeFormat().resolvedOptions().timeZone}`).format()}</p>
-        </div>
+        <article className=" max-w-xl bg-blue-500 rounded-2xl p-4 m-2 self-center hover-float text-slate-50 transition-all">
+            <h1 style={{color : props.amount > 0 ? "green" : "red"}} className=" text-2xl" >{Math.abs(props.amount)}</h1>
+            <h2>{props.from} {"-->"} {props.to}</h2>
+            <h2>
+                <time>{time.date}/{time.months}/{time.years}</time><br/>
+                <time>{time.hours}:{time.minutes}:{time.seconds}</time>
+            </h2>
+        </article>
     );
 }
 
