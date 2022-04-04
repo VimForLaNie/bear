4573e4d09f21324e7d24dc151a4bdfdcd1027f21
VimForLaNie <new.giant098@gmail.com>
2022-04-04 16:11:27 +0700

    deleteTransaction

--- components/displayTransaction.tsx
+++ components/displayTransaction.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import { Ctx } from 'utils/Context';
-import styles from 'styles/components/displayTransaction.module.css';
 import Card from './transactionCard';
 
 const DisplayTransactions = () => {
@@ -11,7 +10,7 @@ const DisplayTransactions = () => {
         <div className="flex flex-col max-w-xl self-center">
           {
             currWallet?.transactions.map((e:transaction,i:number)=>{
-              return <Card key={i} from={e.from} to={e.to} amount={e.amount} date={e.date}></Card>
+              return <Card key={i} from={e.from} to={e.to} amount={e.amount} date={e.date} index={i}></Card>
             }).reverse()
           }
         </div>
--- components/transactionCard.tsx
+++ components/transactionCard.tsx
@@ -1,10 +1,38 @@
 import moment from "moment-timezone";
-import { FC } from "react";
+import { FC, useContext } from "react";
+import { Ctx } from "utils/Context";
+import hashCode from "utils/hash";
+import Post from "utils/post";
 
-const Card:FC<transaction> = (props) => {
+interface Props {
+    from : string,
+    to : string,
+    amount : number,
+    date : number,
+    index : number,
+}
+
+const Card:FC<Props> = (props) => {
     const time = moment.unix(props.date).tz(`${Intl.DateTimeFormat().resolvedOptions().timeZone}`).toObject();
+
+    const { currWallet, wallets, setCurrWallet, setWallets, currIndex, uid } = useContext(Ctx);
+
+    const delTransaction = async (index:number) => {
+        let temp = currWallet;
+        temp.transactions.splice(index, 1);
+        temp.value -= props.amount;
+        setCurrWallet(temp);
+        setWallets([...wallets.slice(0,currIndex), temp, ...wallets.slice(currIndex+1,wallets.length)]);
+
+        window.localStorage.setItem(`data:${hashCode(uid)}`,JSON.stringify(wallets));
+        const res = (await Post(wallets,uid));
+    }
+
     return(
-        <article className=" max-w-xl bg-blue-500 rounded-2xl p-4 m-2 self-center hover-float text-slate-50 transition-all">
+        <article 
+            className=" max-w-xl bg-blue-500 rounded-2xl p-4 m-2 self-center hover-float text-slate-50 transition-all"
+            onClick={() => { delTransaction(props.index) }}
+        >
             <h1 style={{color : props.amount > 0 ? "green" : "red"}} className=" text-2xl" >{Math.abs(props.amount)}</h1>
             <h2>{props.from} {"-->"} {props.to}</h2>
             <h2>
